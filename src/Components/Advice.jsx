28d4f4020b844a6cc3afa0ced79c4e24fab30276
Alejandro Vasquez <josejaguer12@gmail.com>
2024-08-03 07:18:49 -0600

    Done

--- src/Components/Advice.jsx
+++ src/Components/Advice.jsx
@@ -1,4 +1,5 @@
 import Dice from "./../assets/icon-dice.svg";
+import Linea from "./../assets/pattern-divider-desktop.svg";
 
 function Advice({ id, advice, newLoad }) {
   return (
@@ -6,6 +7,8 @@ function Advice({ id, advice, newLoad }) {
       <h2 className="advice-number">Advice #{id}</h2>
 
       <h3 className="advice-text">&quot;{advice}&quot;</h3>
+
+      <img src={Linea} alt="Linea" />
 
       <div className="btn" onClick={() => newLoad()}>
         <img className="" src={Dice} alt="Dado" />
